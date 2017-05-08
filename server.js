db297ee3312140297ebacc1326172bd3b1a8b32e
Alejandro <impera87@gmail.com>
2017-05-08 03:04:01 -0300

    add express to serve example, add babili for minification, add gulp

---
+++ server.js
@@ -0,0 +1,19 @@
+'use strict'
+
+const express = require('express')
+
+const app = express()
+
+app.use('/', express.static(__dirname + '/example'))
+app.use('/lib', express.static(__dirname + '/lib'))
+app.use('/src', express.static(__dirname + '/src'))
+
+app.get('/', function (req, res) {
+  res.sendFile('index.html')
+})
+
+const REST_PORT = (process.env.PORT || 5000)
+
+app.listen(REST_PORT, () => {
+  console.log('App ready on port ' + REST_PORT)
+})
