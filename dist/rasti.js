e6b36909d34630ce8cf0b554d7a3468909e577f4
Alejandro <impera87@gmail.com>
2017-06-23 02:08:23 -0300

    add [move]

--- dist/rasti.js
+++ dist/rasti.js
@@ -835,6 +835,10 @@ var rasti = function(name, container) {
             })
         }
 
+        // init move
+        container.find('[move]').each(function(i, el){
+            $(el).move()
+        })
 
         // init pages
         var page, $page
@@ -1649,6 +1653,62 @@ Array.prototype.remove = function(el) {
 }
 String.prototype.capitalize = function() {
     return this.length && this[0].toUpperCase() + this.slice(1).toLowerCase()
+}
+
+
+// $ extensions
+$.fn.move = function(options) {
+    var options = $.extend({
+            handle: this,
+            container: this.parent()
+        }, options),
+        object = this,
+        newX, newY,
+        nadir = object.css('z-index'),
+        apex = 100000,
+        hold = 'mousedown touchstart',
+        move = 'mousemove touchmove',
+        release = 'mouseup touchend'
+
+    if (!object[0].hasAttribute('move')) object.attr('move', '')
+
+    options.handle.on(hold, function(e) {
+        if (e.type == 'mousedown' && e.which != 1) return
+        object.css('z-index', apex)
+        var marginX = options.container.width() - object.width(),
+            marginY = options.container.height() - object.height(),
+            oldX = object.position().left,
+            oldY = object.position().top,
+            touch = e.touches,
+            startX = touch ? touch[0].pageX : e.pageX,
+            startY = touch ? touch[0].pageY : e.pageY
+
+        $(window)
+            .on(move, function(e) {
+                var touch = e.touches,
+                    endX = touch ? touch[0].pageX : e.pageX,
+                    endY = touch ? touch[0].pageY : e.pageY
+                newX = Math.max(0, Math.min(oldX + endX - startX, marginX))
+                newY = Math.max(0, Math.min(oldY + endY - startY, marginY))
+
+                window.requestAnimationFrame
+                    ? requestAnimationFrame(setElement)
+                    : setElement()
+            })
+            .one(release, function(e) {
+                e.preventDefault()
+                object.css('z-index', nadir)
+                $(this).off(move).off(release)
+            })
+
+        e.preventDefault()
+    })
+
+    return this
+
+    function setElement() {
+        object.css({top: newY, left: newX});
+    }
 }
 
 
