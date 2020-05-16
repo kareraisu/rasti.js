9dc5ba97190f9e8e2367c911e7d5d9e97e497161
kareraisu <impera87@gmail.com>
2020-05-16 17:55:13 -0300

    add chain(), safe() and is.primitive() utility functions improve inject() (add stylesheets support)

--- src/utils.js
+++ src/utils.js
@@ -3,11 +3,15 @@ function type(exp) {
     return clazz.substring(8, clazz.length-1).toLowerCase()
 }
 
+const primitives = 'string number boolean symbol'
+const all_types = 'object function array regexp ' + primitives
+
 const is = {}
-'object function array string number regex boolean'.split(' ')
+all_types.split(' ')
     .forEach(t => {
         is[t] = exp => type(exp) === t
     })
+is.primitive = exp => primitives.includes( type(exp) )
 is.empty = exp =>
     (is.array(exp) || is.string(exp)) ? exp.length === 0
     : is.object(exp) ? Object.keys(exp).length === 0
@@ -23,53 +27,78 @@ const sameType = (exp1, exp2) => type(exp1) === type(exp2)
 const compose = (...funcs) => funcs.reduce((prev, curr) => (...args) => curr(prev(...args)))
 
 
+const chain = ref => method => (...args) => { method(...args); return ref }
+
+
+const safe = err_handler => method => (...args) => {
+    try { method(...args) }
+    catch(err) { is.array(err) ? err_handler(...err) : err_handler(err) }
+}
+
+
 const prepTemplate = tmpl_func => data => data.map( compose( checkData, tmpl_func )).join('')
 
 
 function inject(sources) {
     if (is.string(sources)) sources = sources.split(',')
     if (is.not.array(sources)) return rasti.error('Invalid sources, must be an array or a string')
-    const body = $('body')
+    const $body = $(document.body)
+
     function do_inject(sources) {
         const src = sources.shift().trim()
-        const script = $('<script>').attr('src', src)
-        script[0].onload = () => {
+        const ext = src.split('.')[1]
+        let $el
+
+        switch (ext) {
+            case 'js':
+                $el = $('<script>').attr('src', src)
+                break
+            case 'css':
+                $el = $('<link rel=stylesheet>').attr('href', src)
+                break
+            default:
+                rasti.error('Invalid source "%s", must be a js script or a css stylesheet', src)
+                return
+        }
+  
+        $el[0].onload = () => {
             rasti.log('> Loaded %s', src)
             sources.length
                 ? do_inject(sources)
                 : rasti.log('All sources loaded')
         }
         rasti.log('> Loading %s ...', src)
-        body.append(script)
+        $body.append($el)
     }
+
     do_inject(sources)
 }
 
 
 function checkData(data) {
     switch (typeof data) {
-    case 'string':
-        data = {value: data, label: data, alias: data.toLowerCase()}
-        break
-    case 'object':
-        if ( is.not.string(data.value) || is.not.string(data.label) ) {
-            rasti.error('Invalid data object (must have string properties "value" and "label"):', data)
+        case 'string':
+            data = {value: data, label: data, alias: data.toLowerCase()}
+            break
+        case 'object':
+            if ( is.not.string(data.value) || is.not.string(data.label) ) {
+                rasti.error('Invalid data object (must have string properties "value" and "label"):', data)
+                //invalid_count++
+                data = {value: '', label: 'INVALID DATA', alias: ''}
+            }
+            else if ( is.not.string(data.alias) ) {
+                if (data.alias) {
+                    rasti.error('Invalid data property "alias" (must be a string):', data)
+                    //invalid_count++
+                }
+                data.alias = data.value.toLowerCase()
+            }
+            else data.alias = data.alias.toLowerCase() +' '+ data.value.toLowerCase()
+            break
+        default:
+            rasti.error('Invalid data (must be a string or an object):', data)
             //invalid_count++
             data = {value: '', label: 'INVALID DATA', alias: ''}
-        }
-        else if ( is.not.string(data.alias) ) {
-            if (data.alias) {
-                rasti.error('Invalid data property "alias" (must be a string):', data)
-                //invalid_count++
-            }
-            data.alias = data.value.toLowerCase()
-        }
-        else data.alias = data.alias.toLowerCase() +' '+ data.value.toLowerCase()
-        break
-    default:
-        rasti.error('Invalid data (must be a string or an object):', data)
-        //invalid_count++
-        data = {value: '', label: 'INVALID DATA', alias: ''}
     }
     return data
 }
@@ -184,7 +213,9 @@ const public = {
     inject,
     random,
     compose,
-    keyNav
+    chain,
+    safe,
+    keyNav,
 }
 
 const private = {
@@ -197,5 +228,5 @@ const private = {
 module.exports = {
     ...public,
     ...private,
-    public
+    public,
 }
